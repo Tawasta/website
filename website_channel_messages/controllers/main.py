e499ef1bd749082395d5c3f8c761d2b7b9c059a5
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2019-12-05 13:38:19 +0200

    [ADD] WIP: Add channels to website

---
+++ website_channel_messages/controllers/main.py
@@ -0,0 +1,99 @@
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2019- Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
+from odoo import http
+from odoo.http import request
+
+# 4. Imports from Odoo modules (rarely, and only if necessary):
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports (One per line sorted and splitted in
+
+
+class WebsiteChannelMessagesController(http.Controller):
+
+    @http.route(
+        ['/website_channels', '/website_channels/page/<int:page>'],
+        type='http',
+        auth='user',
+        website=True,
+    )
+    def channel_messages(self, search='', page=1, **post):
+        """
+        Route to show list of all channels.
+
+        :param search: Search query
+        :param page: pager current page
+        :param post: kwargs
+        :return: rendered object
+        """
+        partner_id = request.env.user.partner_id.id
+        channel_model = request.env['mail.channel']
+
+        if not partner_id:
+            return request.render('website.404')
+
+        domain = [
+            ('channel_type', '=', 'chat'),
+            ('name', 'ilike', search),
+        ]
+        channel_count = channel_model.search_count(domain)
+        url = request.httprequest.path.split("/page")[0]
+        total_count = channel_count
+        pager_limit = 50
+        pager = request.website.pager(
+            url=url,
+            total=total_count,
+            page=page,
+            step=pager_limit,
+            scope=7,
+            url_args=post
+        )
+        channels = channel_model.sudo().search(
+            domain,
+            order="id DESC",
+            limit=pager_limit,
+            offset=pager['offset']
+        )
+        search_url = request.httprequest.path + ("?%s" % search)
+        message_start = abs(50 - page * pager_limit) + 1
+        message_end = total_count if total_count < page * pager_limit else page * pager_limit
+        visible = "{} - {} / {}".format(message_start, message_end, total_count)
+
+        print("--------------------")
+        print(channels)
+        values = {
+            'channels': channels,
+            'pager': pager,
+            'visible_channels': visible,
+            'search_url': search_url,
+            'current_search': search,
+        }
+        return request.render(
+            'website_channel_messages.my_channels',
+            values
+        )
