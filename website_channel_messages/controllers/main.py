1bdd1f022ab26a9d90d7fb4ebb3f348c8d7bce98
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2019-12-09 15:16:38 +0200

    [ADD] WIP: Add thread, submission form and CKEditor
    TODO: Attachment processing, filesize limitations etc...

--- website_channel_messages/controllers/main.py
+++ website_channel_messages/controllers/main.py
@@ -37,12 +37,12 @@ from odoo.http import request
 class WebsiteChannelMessagesController(http.Controller):
 
     @http.route(
-        ['/website_channels', '/website_channels/page/<int:page>'],
+        ['/website_channel', '/website_channel/page/<int:page>'],
         type='http',
         auth='user',
         website=True,
     )
-    def channel_messages(self, search='', page=1, **post):
+    def website_channels(self, search='', page=1, **post):
         """
         Route to show list of all channels.
 
@@ -58,8 +58,12 @@ class WebsiteChannelMessagesController(http.Controller):
             return request.render('website.404')
 
         domain = [
-            ('channel_type', '=', 'chat'),
+            ('channel_type', '=', 'channel'),
+            ('public', '=', 'private'),
+            ('channel_last_seen_partner_ids.partner_id', '=', partner_id),
+            '|',
             ('name', 'ilike', search),
+            ('channel_last_seen_partner_ids.partner_id', 'ilike', search),
         ]
         channel_count = channel_model.search_count(domain)
         url = request.httprequest.path.split("/page")[0]
@@ -84,8 +88,6 @@ class WebsiteChannelMessagesController(http.Controller):
         message_end = total_count if total_count < page * pager_limit else page * pager_limit
         visible = "{} - {} / {}".format(message_start, message_end, total_count)
 
-        print("--------------------")
-        print(channels)
         values = {
             'channels': channels,
             'pager': pager,
@@ -97,3 +99,67 @@ class WebsiteChannelMessagesController(http.Controller):
             'website_channel_messages.my_channels',
             values
         )
+
+    @http.route(
+        ['/website_channel/<int:channel_id>'],
+        type='http',
+        auth='user',
+        website=True,
+    )
+    def channel_messages(self, channel_id=None, **post):
+        """
+        Route to show list of all channels.
+
+        :param channel_id: Related channel ID
+        :param post: kwargs
+        :return: rendered object
+        """
+        partner_id = request.env.user.partner_id.id
+        channel = request.env['mail.channel'].sudo().search([
+            ('id', '=', channel_id),
+            ('channel_type', '=', 'channel'),
+            ('public', '=', 'private'),
+            ('channel_last_seen_partner_ids.partner_id', '=', partner_id),
+        ])
+
+        if not channel:
+            return request.render('website.404')
+
+        values = {
+            'channel': channel,
+        }
+        return request.render(
+            'website_channel_messages.channel',
+            values
+        )
+
+    @http.route(
+        ['/website_channel/<int:channel_id>/message'],
+        type='http',
+        auth="user",
+        website=True,
+        methods=['POST']
+    )
+    def channel_send_message(self, channel_id=None, **post):
+        """
+        Message submission handler for channel
+
+        :param channel_id: related channel ID
+        :param post: submitted form payload
+        """
+        user = request.env.user
+        channel = request.env['mail.channel'].sudo().search([
+            ('id', '=', channel_id),
+            ('channel_last_seen_partner_ids.partner_id', '=', user.partner_id.id),
+        ])
+
+        if not channel:
+            return request.render('website.404')
+
+        redirect_url = post.get('redirect_url')
+        channel.sudo(user).message_post(
+            body=post.get('comment'),
+            message_type='comment',
+            subtype='mail.mt_comment',
+        )
+        return request.redirect(redirect_url)
