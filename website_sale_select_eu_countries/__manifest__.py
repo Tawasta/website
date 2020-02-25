36b91aca7027db48bd68ceee82861e11614a8017
difbit <timo.kekalainen@hotmail.com>
2020-02-25 15:00:34 +0200

    [IMP] Correct author

--- website_sale_select_eu_countries/__manifest__.py
+++ website_sale_select_eu_countries/__manifest__.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-#    Author: Oy Tawasta OS Technologies Ltd.
+#    Author: Tawasta
 #    Copyright 2019 Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
 #
 #    This program is free software: you can redistribute it and/or modify
