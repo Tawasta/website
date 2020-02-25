ad79fc2b5011bd6ddf96c9eccce1738b69788d0f
difbit <timo.kekalainen@hotmail.com>
2020-02-25 15:00:45 +0200

    [IMP] New class name

--- website_sale_select_eu_countries/controllers/portal.py
+++ website_sale_select_eu_countries/controllers/portal.py
@@ -3,11 +3,11 @@ from odoo.http import request, route
 from odoo.addons.portal.controllers.portal import CustomerPortal
 
 
-class Extension(CustomerPortal):
+class AccountCountries(CustomerPortal):
 
     @route()
     def account(self, redirect=None, **post):
-        res = super(Extension, self).account(redirect=None, **post)
+        res = super(AccountCountries, self).account(redirect=None, **post)
         europe = request.env.ref('base.europe').country_ids
         res.qcontext['countries'] = europe
         return res
