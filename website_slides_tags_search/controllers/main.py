22bbc110e7bcbc897436600e15c1dcb544912116
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2020-12-18 07:52:55 +0200

    Modified tag search

--- website_slides_tags_search/controllers/main.py
+++ website_slides_tags_search/controllers/main.py
@@ -8,67 +8,7 @@ from odoo.http import request
 from odoo.addons.website_slides.controllers.main import WebsiteSlides
 
 
-def slide_filter(response, filter_tags):
-    # Remove any slides without any given tags
-    def tag_filter(slide):
-        print("-------------------KÄYKÖ TÄÄÄLLÄ----------")
-        print(slide)
-        print(slide.tag_ids)
-        slides_tags = slide.tag_ids.mapped("name")
-        print(slides_tags)
-        return all(tag in slides_tags for tag in filter_tags)
-
-    response.qcontext['slides'] = list(filter(
-        tag_filter,
-        list(response.qcontext['slides'])
-    ))
-
-    return response
-
-
 class SlidesSearchExtended(WebsiteSlides):
-    # @http.route()
-    # def channel(self, *args, **kw):
-    #     print("-------------------KÄYKÖ TÄÄÄLLÄ 2----------")
-
-    #     response = super(SlidesSearchExtended, self).channel(*args, **kw)
-
-    #     search_tags = kw.get('tags_search')
-    #     if search_tags:
-    #         li = search_tags.strip("[]").replace('"', "").replace(" ", "").split(",")
-    #         tags = request.env["slide.tag"].sudo().search([
-    #             ('id', 'in', li)
-    #         ])
-    #         print(tags)
-    #         response.qcontext['last_chosen_tags'] = tags
-    #         response = slide_filter(response, tags)
-
-    #     # search_tags = kw.get('tags_search')
-    #     # print("----------SEARCH TAGS-------")
-    #     # print(search_tags)
-    #     # print("----------SEARCH TAGS-------")
-    #     # if search_tags:
-    #     #     print("--------------------YOLOOOO-------")
-    #     #     print(search_tags)
-    #     #     print(int(search_tags))
-    #     #     tags = request.env["slide.tag"].sudo().search([
-    #     #         ('id', 'in', search_tags)
-    #     #     ])
-    #     #     slides = request.env["slide.slide"].sudo().search([
-    #     #         ('tag_ids', 'in', tags)
-    #     #     ])
-    #     #     print(slides)
-    #     # search = kw.get('search')
-    #     # if not search:
-    #     #     search_tags = None
-
-    #     # if search_tags:
-    #     #     print("---------------------IF SEARCH TAGS---------------")
-    #     #     print(search_tags)
-    #     #     response.qcontext['last_chosen_tags'] = search_tags
-    #     #     # response = slide_filter(response, search_tags.split(","))
-
-    #     return response
 
     @http.route([
         '''/slides/<model("slide.channel"):channel>''',
